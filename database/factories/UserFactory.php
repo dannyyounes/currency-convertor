080f1ba88d00410d0120226c1e7385faf2386093
Danny Younes <dannyyounes@gmail.com>
2022-05-09 08:48:58 +1000

    update to user factory password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory
             'last_name' => $this->faker->lastName(),
             'email' => 'dannyyounes@gmail.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$1hMzvaOTuxxEv2VZRtM.U.VfetwGuPZpQugyEnEEv/kprWTkJiG9i', // password
             'remember_token' => Str::random(10),
         ];
     }
